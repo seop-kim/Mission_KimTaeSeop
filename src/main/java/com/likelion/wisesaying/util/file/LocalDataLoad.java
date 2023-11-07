3adfff192b03726355de5aade24a7a817b5ce0f7
tseop <77163842+seop-kim@users.noreply.github.com>
2023-11-07 18:22:54 +0900

    feat: Json 데이터 저장 기능 구현

--- src/main/java/com/likelion/wisesaying/util/file/LocalDataLoad.java
+++ src/main/java/com/likelion/wisesaying/util/file/LocalDataLoad.java
@@ -2,21 +2,24 @@ package com.likelion.wisesaying.util.file;
 
 import com.likelion.wisesaying.domain.Saying;
 import com.likelion.wisesaying.service.SayingService;
+import com.likelion.wisesaying.util.generator.IdGenerator;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
 
 public class LocalDataLoad {
     private static final String USER_HOME_PATH = System.getProperty("user.home");
-    private static final String FILE_PATH =  USER_HOME_PATH +"/workspace/saveWiseData.txt";
+    private static final String FILE_PATH = USER_HOME_PATH + "/workspace/saveSayingsData.txt";
     private final SayingService service = new SayingService();
+    private final IdGenerator generator = new IdGenerator();
+
     public void load() {
         System.out.println(FILE_PATH);
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH));
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH))) {
             String line = "";
 
-            // txt 내 텍스트 전체를 읽을때까지 반복문을 통해 데이터 로드하여 arr에 데이터 저장
             while ((line = reader.readLine()) != null) {
                 String[] len = line.split(",");
 
@@ -30,12 +33,13 @@ public class LocalDataLoad {
                     saying.setAuthor(loadAuthor);
                     saying.setContent(loadSentence);
 
-                    service.save(saying);
+                    service.load(saying);
                 }
             }
 
         } catch (IOException e) {
             System.out.println(e.getMessage());
+
         }
     }
 }
--- src/main/java/com/likelion/wisesaying/util/file/LocalDataSave.java
+++ src/main/java/com/likelion/wisesaying/util/file/LocalDataSave.java
@@ -8,26 +8,36 @@ import java.io.IOException;
 
 public class LocalDataSave {
     private static final String USER_HOME_PATH = System.getProperty("user.home");
-    private static final String FILE_PATH =  USER_HOME_PATH +"/workspace/saveWiseData.txt";
+    private static final String FILE_PATH = USER_HOME_PATH + "/workspace";
     private static final SayingService service = new SayingService();
-    public void save() {
-        try {
-            // 파일 생성 및 텍스트 작성을 위한 버퍼드 객체 생성
-            BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_PATH));
 
-            // 반복문을 통해 wiseArr에 들어있는 데이터를 txt 파일에 순차적 작성
-            for (Saying saying : service.findAll()) {
+    public void saveTxt() {
+        String fileName = "/saveSayingsData.txt";
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_PATH + fileName))) {
+
+            for (Saying saying : service.findAllReverse()) {
                 String saveData =
                         String.format("%d,%s,%s", saying.getId(), saying.getAuthor(),
-                        saying.getContent());
+                                saying.getContent());
                 writer.write(saveData);
                 writer.newLine();
             }
 
-            // 버퍼드 객체 close
-            writer.close();
         } catch (IOException e) {
             System.out.println(e.getMessage());
+
+        }
+    }
+
+    public void saveJson(String jsonStr) {
+        String fileName = "/saveJsonSayings.json";
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_PATH + fileName))) {
+            writer.write(jsonStr);
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+
         }
     }
 }
