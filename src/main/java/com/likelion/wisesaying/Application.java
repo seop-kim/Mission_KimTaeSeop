f568b714cc3211f1b923d5cdfd9e0997f391454e
tseop <77163842+seop-kim@users.noreply.github.com>
2023-11-07 10:49:50 +0900

    feat: 고유번호 자동증가 기능 구현

--- src/main/java/com/likelion/wisesaying/Application.java
+++ src/main/java/com/likelion/wisesaying/Application.java
@@ -1,10 +1,12 @@
 package com.likelion.wisesaying;
 
 import com.likelion.wisesaying.domain.Saying;
+import com.likelion.wisesaying.service.SayingService;
 import java.util.Scanner;
 
 public class Application {
     private final Scanner sc = new Scanner(System.in);
+    private  final SayingService service = new SayingService();
     public void run() {
         String request = "";
         System.out.println("== 명언 앱 ==");
@@ -22,11 +24,12 @@ public class Application {
                 String requestAuthor = sc.nextLine();
 
                 Saying saying = new Saying();
-                saying.setId(1L);
                 saying.setContent(requestContent);
                 saying.setAuthor(requestAuthor);
 
-                System.out.println(saying.getId() + "번 명언이 등록되었습니다.");
+                Long saveId = service.save(saying);
+
+                System.out.println(saveId + "번 명언이 등록되었습니다.");
             }
         }
     }
