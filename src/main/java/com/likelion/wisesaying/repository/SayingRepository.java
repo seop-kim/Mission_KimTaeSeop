2e0de831fbeb0f222400d86d084e5be211da0479
tseop <77163842+seop-kim@users.noreply.github.com>
2023-11-07 16:35:33 +0900

    feat: 레포지토리 삭제 기능 추가

--- src/main/java/com/likelion/wisesaying/repository/SayingRepository.java
+++ src/main/java/com/likelion/wisesaying/repository/SayingRepository.java
@@ -5,15 +5,13 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import lombok.Getter;
 
 public class SayingRepository {
+    @Getter
     private static final SayingRepository instance = new SayingRepository();
     private final Map<Long, Saying> sayings = new HashMap<>();
     private SayingRepository() {
-    }
-
-    public static SayingRepository getInstance() {
-        return instance;
     }
 
     public void save(Saying saying) {
@@ -22,5 +20,12 @@ public class SayingRepository {
 
     public List<Saying> findAll() {
         return new ArrayList<>(sayings.values());
+    }
+
+    public Saying findOne(Long id) {
+        return sayings.get(id);
+    }
+    public void delete(Long id) {
+        sayings.remove(id);
     }
 }
