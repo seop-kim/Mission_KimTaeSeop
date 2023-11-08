2bc64986bc0859c5ef3c22fe13358bcf8446d99b
tseop <77163842+seop-kim@users.noreply.github.com>
2023-11-08 14:07:29 +0900

    refactor: 단일책임원칙에 따른 기능 분리

--- src/main/java/com/likelion/wisesaying/util/convertor/RequestConverter.java
+++ src/main/java/com/likelion/wisesaying/util/convertor/RequestConverter.java
@@ -3,13 +3,12 @@ package com.likelion.wisesaying.util.convertor;
 import com.likelion.wisesaying.util.exception.CustomRequestException;
 
 public class RequestConverter {
-    private final TypeConverter typeConverter = new TypeConverter();
-    public Long splitId(String request) throws IndexOutOfBoundsException {
-        String[] splitStr = request.split("\\?id=");
-        try {
-            return typeConverter.strToLong(splitStr[1]);
-        } catch (IndexOutOfBoundsException e) {
-            throw new CustomRequestException();
+
+    public String[] splitId(String request) throws IndexOutOfBoundsException {
+        if (request.contains("?id=")) {
+            return request.split("\\?id=");
         }
+
+        return new String[]{request, ""};
     }
 }
