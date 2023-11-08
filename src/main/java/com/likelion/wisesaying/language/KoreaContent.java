23f10645723adbbbc89beda31ea9856869d0f6f7
tseop <77163842+seop-kim@users.noreply.github.com>
2023-11-08 14:09:40 +0900

    refactor: 텍스트 상수 클래스 생성

---
+++ src/main/java/com/likelion/wisesaying/language/KoreaContent.java
@@ -0,0 +1,27 @@
+package com.likelion.wisesaying.language;
+
+public class KoreaContent {
+    public static final String START_MENU = "== 명언 앱 ==";
+    public static final String REQUEST_MENU = "명령) ";
+    public static final String NONE_COMMEND = "존재하지 않는 명령어입니다.";
+    public static final String REGISTER = "등록";
+    public static final String UPDATE = "수정";
+    public static final String LIST = "목록";
+    public static final String DELETE = "삭제";
+    public static final String BUILD = "빌드";
+    public static final String END = "종료";
+
+    public static final String DELETE_MSG = "번 명언이 삭제되었습니다.";
+
+    public static final String LIST_GUIDE_MSG = "번호\t/\t작가\t/\t명언\n----------------------";
+
+    public static final String REQUEST_CONTENT = "명언 : ";
+    public static final String REQUEST_AUTHOR = "작가 : ";
+
+    public static final String RESISTER_SUCCESS_MSG = "번 명언이 등록되었습니다.";
+
+    public static final String UPDATE_REQUEST_CONTENT = "명언(기존) : ";
+    public static final String UPDATE_REQUEST_AUTHOR = "작가(기존) : ";
+
+    public static final String NONE_FIND_DATA = "번 명언은 존재하지 않습니다.";
+}
