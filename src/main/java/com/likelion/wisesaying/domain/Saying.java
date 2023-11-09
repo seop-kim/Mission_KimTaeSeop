8d5dfb8aa438e8d6bcab261982403dd3422f7f86
tseop <77163842+seop-kim@users.noreply.github.com>
2023-11-09 11:59:29 +0900

    refactor: Add constructor

--- src/main/java/com/likelion/wisesaying/domain/Saying.java
+++ src/main/java/com/likelion/wisesaying/domain/Saying.java
@@ -10,9 +10,26 @@ public class Saying {
     private String content;
     private String author;
 
+    public Saying() {
+    }
+
+    public Saying(String content, String author) {
+        this.content = content;
+        this.author = author;
+    }
+
+    public Saying(Long id, String content, String author) {
+        this.id = id;
+        this.content = content;
+        this.author = author;
+    }
+
+    public void updateSaying(String content, String author) {
+        this.content = content;
+        this.author = author;
+    }
     @Override
     public String toString() {
         return id + "\t/\t" + author + "\t/\t" + content;
-
     }
 }
