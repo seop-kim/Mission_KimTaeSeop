c70a70356a77ff9339a54f68af797de2d0760753
tseop <77163842+seop-kim@users.noreply.github.com>
2023-11-09 11:59:41 +0900

    refactor: Modify eof

--- src/main/java/com/likelion/wisesaying/language/KoreaContent.java
+++ src/main/java/com/likelion/wisesaying/language/KoreaContent.java
@@ -2,13 +2,21 @@ package com.likelion.wisesaying.language;
 
 public class KoreaContent {
     public static final String START_MENU = "== 명언 앱 ==";
+
     public static final String REQUEST_MENU = "명령) ";
+
     public static final String NONE_COMMEND = "존재하지 않는 명령어입니다.";
+
     public static final String REGISTER = "등록";
+
     public static final String UPDATE = "수정";
+
     public static final String LIST = "목록";
+
     public static final String DELETE = "삭제";
+
     public static final String BUILD = "빌드";
+
     public static final String END = "종료";
 
     public static final String DELETE_MSG = "번 명언이 삭제되었습니다.";
@@ -16,14 +24,22 @@ public class KoreaContent {
     public static final String LIST_GUIDE_MSG = "번호\t/\t작가\t/\t명언\n----------------------";
 
     public static final String REQUEST_CONTENT = "명언 : ";
+
     public static final String REQUEST_AUTHOR = "작가 : ";
 
     public static final String RESISTER_SUCCESS_MSG = "번 명언이 등록되었습니다.";
 
     public static final String UPDATE_REQUEST_CONTENT = "명언(기존) : ";
+
     public static final String UPDATE_REQUEST_AUTHOR = "작가(기존) : ";
 
     public static final String NONE_FIND_DATA = "번 명언은 존재하지 않습니다.";
 
     public static final String NONE_REPO_ADAPTER = "존재하지 않는 저장방식 입니다.";
+
+    public static final String SQL_CON_FAIL = "SQL 연결 실패.";
+
+    public static final String NO_SUCH_JDBC_DRIVER = "JDBC 드라이버를 찾을 수 없습니다.";
+
+    public static final String MYSQL_CONNECTING_ERROR = "MySQL에 연결하는 동안 오류가 발생했습니다.";
 }
