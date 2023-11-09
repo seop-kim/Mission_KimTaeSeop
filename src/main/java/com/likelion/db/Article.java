6dc5a0a3632d8950d1a941db1c7f247f1b25a858
tseop <77163842+seop-kim@users.noreply.github.com>
2023-11-09 11:57:53 +0900

    refactor: Modify eof

--- src/main/java/com/likelion/db/Article.java
+++ src/main/java/com/likelion/db/Article.java
@@ -6,9 +6,15 @@ import lombok.Data;
 @Data
 public class Article {
     private long id;
+
     private LocalDateTime createdDate;
+
     private LocalDateTime modifiedDate;
+
     private String title;
+
     private String body;
+
     private boolean isBlind;
+
 }
