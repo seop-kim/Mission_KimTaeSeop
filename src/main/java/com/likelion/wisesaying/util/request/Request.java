96bdfe0e8d8ed8f77eb44966bea55d70994031af
tseop <77163842+seop-kim@users.noreply.github.com>
2023-11-09 12:03:37 +0900

    refactor: 메인에 있는 getRequest 이동

--- src/main/java/com/likelion/wisesaying/util/request/Request.java
+++ src/main/java/com/likelion/wisesaying/util/request/Request.java
@@ -1,5 +1,6 @@
 package com.likelion.wisesaying.util.request;
 
+import com.likelion.wisesaying.language.KoreaContent;
 import com.likelion.wisesaying.util.convertor.RequestConverter;
 import java.util.Map;
 import java.util.Scanner;
@@ -9,16 +10,22 @@ public class Request {
         return new Scanner(System.in).nextLine();
     }
 
-    public static Map<String, String> setModel(Map<String, String> request) {
+    public static String input(Map<String, String> model) {
+        System.out.print(KoreaContent.REQUEST_MENU);
+        String request = Request.input();
+        model.put("request", request);
+        Request.setModel(model);
+        return request;
+    }
+
+    private static void setModel(Map<String, String> model) {
         RequestConverter RqConverter = new RequestConverter();
-        String[] rq = RqConverter.splitId(request.get("request"));
+        String[] rq = RqConverter.splitId(model.get("request"));
 
         String path = rq[0];
         String id = rq[1];
 
-        request.put("path", path);
-        request.put("id", id);
-
-        return request;
+        model.put("path", path);
+        model.put("id", id);
     }
 }
