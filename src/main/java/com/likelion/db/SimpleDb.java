b0a3f61b98b4ff55e02602554607b7cd8e2c6c28
seop <77163842+seop-kim@users.noreply.github.com>
2024-03-27 14:05:01 +0900

    1

--- src/main/java/com/likelion/db/SimpleDb.java
+++ src/main/java/com/likelion/db/SimpleDb.java
@@ -1,50 +1,28 @@
 package com.likelion.db;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class SimpleDb {
-    String jdbcUrl;
-    String username;
-    String password;
 
-    public SimpleDb(String host, String username, String password, String dbName) {
-        this.jdbcUrl = "jdbc:mysql://" + host + ":3306/" + dbName;
+    private final String url;
+    private final String username;
+    private final String password;
+    private final String dbName;
+    private Connection connection;
+
+    public SimpleDb(String url, String username, String password, String dbName) {
+        this.url = url;
         this.username = username;
         this.password = password;
+        this.dbName = dbName;
     }
 
-    public static void main(String[] args) {
-        SimpleDb simpleDB = new SimpleDb("localhost", "root", "lldj123414", "simpleDb__test");
-        simpleDB.connect();
-    }
-    public void connect() {
-        try {
-            // MySQL JDBC 드라이버를 로드합니다.
-            Class.forName("com.mysql.cj.jdbc.Driver");
-
-            // 데이터베이스에 연결합니다.
-            Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
-
-            if (connection != null) {
-                System.out.println("MySQL 연결 성공!");
-                connection.close();
-            } else {
-                System.out.println("MySQL 연결 실패.");
-            }
-        } catch (ClassNotFoundException e) {
-            System.err.println("JDBC 드라이버를 찾을 수 없습니다.");
-            e.printStackTrace();
-
-        } catch (SQLException e) {
-            System.err.println("MySQL에 연결하는 동안 오류가 발생했습니다.");
-            e.printStackTrace();
-        }
+    public void setDevMode(boolean devMode) {
+        // 개발 모드를 설정하는 메소드입니다.
+        // 이것은 필요에 따라 구현해야 할 부분입니다.
     }
 
     public void run(String sql) {
 
     }
 }
-
---
+++ src/test/java/com/likelion/wisesaying/SimpleDbTest.java
@@ -0,0 +1,8 @@
+package com.likelion.wisesaying;
+
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
+
+import org.junit.jupiter.api.TestInstance;
+
+@TestInstance(PER_CLASS)
+public class SimpleDbTest {}
